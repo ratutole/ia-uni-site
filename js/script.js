7eb8ea4e01eebfb0208ba4dad056b9f9709a0e08
Parth Patel <parthcdgsa@gmail.com>
2018-10-27 19:49:58 +0530

    implementation of carusel start

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+
+// Variables navbar
 const MENU = document.querySelector('.nav-toggler');
 const MENUITEMS = document.querySelector('.mobile-nav');
 
+// Variables research slider
+const CARUSELS = document.querySelectorAll('.carusel');
+
+// Responsive Navigation
 MENU.addEventListener('click', function(){
     MENUITEMS.classList.toggle('menu-hidden');
 });
+
